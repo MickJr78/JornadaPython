42437cef954b91d1114e2bdb8fc0f1d8afa817e9
MickJr78 <jbarbosajr2018@gmail.com>
2024-11-15 06:47:55 -0300

    Fatec EDU - Exercícios diversos em Python

--- ExerciciosPython/ExercicioProposto4-8.py
+++
@@ -1,8 +0,0 @@
-# AVALIA SE UM ANO É OU NÃO BISSEXTO
-
-ano = int(input('Digite o ano em questão: '))
-
-if (ano % 4 == 0) and(ano %100 != 0) or (ano % 400 == 0):
-    print(f'O ano de {ano} é bissexto.')
-else:
-    print(f'O ano de {ano} NÃO é bissexto.')
